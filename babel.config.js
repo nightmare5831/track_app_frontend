f4e0f62d37d7212b4e43777f00869b6e059b6bdf
nightmare <jastinmax888@gmail.com>
2025-11-28 02:32:53 +0200

    feat:complete setting for build apk

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,7 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
-    plugins: [
-      "react-native-worklets/plugin",
-      "react-native-reanimated/plugin",
-    ],
+    presets: ['babel-preset-expo'],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
